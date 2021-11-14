5ce94ef15f071b5d25a253d17538052cb223ced4
jakethesnake003 <58544487+jakethesnake003@users.noreply.github.com>
2021-11-15 07:53:56 +1300

    add login/register to correct forms

--- php/user/user_create.php
+++ php/user/user_create.php
@@ -3,20 +3,20 @@ include '../../../auth.php';
 
 $email = $_REQUEST["email"];
 $password = $_REQUEST["password"];
+$fname = $_REQUEST["fname"];
+$lname = $_REQUEST["lname"];
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $db_username, $db_password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $query = mysql_query("INSERT INTO `users` (`email`, `password`) VALUES ('$email', '$password')");
-  if(mysql_num_rows($query) == 0){
-    echo "user found";
+  $conn = mysqli_connect($servername, $db_username, $db_password, $dbname);
+  $query = mysqli_query($conn, "SELECT * FROM users WHERE (email = '$email')");
+  if(mysqli_num_rows($query) == 0){
+    mysqli_query($conn, "INSERT INTO `users` (`email`, `password`, `firstname`, `lastname`) VALUES ('$email', '$password', '$fname', '$lname')");
+    echo "user created";
   } else {
-    echo ""
+    echo "already in database";
   }
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
-
 $conn = null;
 ?>
--- php/user/user_login.php
+++ php/user/user_login.php
@@ -6,13 +6,11 @@ $password = $_REQUEST["password"];
 
 try {
   $conn = mysqli_connect($servername, $db_username, $db_password, $dbname);
-  // set the PDO error mode to exception
   $query = mysqli_query($conn, "SELECT * FROM users WHERE (email = '$email') AND (password = '$password')");
   if(mysqli_num_rows($query) == 0){
     echo "Invalid username / password";
   } else {
-    session_start();  
-    $_SESSION['sess_user']=$email;
+    echo "Login successful";
   }
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
